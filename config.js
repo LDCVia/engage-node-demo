9f0362ee8f5222914701a4348582c2df8506df0f
Benedict Poole <site@benpoole.com>
2016-03-19 16:02:08 +0000

    Moved config, added live and dev vars

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  apihost: "https://eu.ldcvia.com/1.0",
-  db: "dev-londc-com-demos-200kpeople-nsf1444113120236",
-  title: "Engage node.js LDC Via Demo",
-  secret: "winkles"
-};
---
+++ config/config.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  module.exports = {
+    LIVEapihost: "https://eu.ldcvia.com/1.0",
+    apihost: "https://local.ldcvia.com:3001/1.0",
+    LIVEdb: "dev-londc-com-demos-200kpeople-nsf1444113120236",
+    db: "dev-200kpeople-nsf",
+    title: "Engage node.js LDC Via Demo",
+    secret: "winkles"
+  };
+}());
