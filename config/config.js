358e423920c42fbbf1355345fef8e9ac9e6b15a2
Ben Poole <benpoole@users.noreply.github.com>
2016-04-04 15:21:16 +0100

    Changed app title

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@
   module.exports = {
     apihost: "https://eu.ldcvia.com/1.0",
     db: "dev-londc-com-demos-200kpeople-nsf1444113120236",
-    title: "Engage node.js LDC Via Demo",
+    title: "node.js / Express LDC Via Demo",
     secret: "your_secret_here"
   };
 }());
